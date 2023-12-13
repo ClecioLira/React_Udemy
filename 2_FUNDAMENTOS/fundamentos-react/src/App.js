ebea9967d20a18362efbfe4069d8b4858e1f5ff8
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-12-13 17:58:19 -0300

    update component

---
+++ 2_FUNDAMENTOS/fundamentos-react/src/App.js
@@ -0,0 +1,16 @@
+// componente
+import FirstComponent from './component/firstComponent';
+
+// estilo
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos do React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
