28c31e9270af63532abc8fbe6e9603e6b92895f1
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-12-21 14:40:07 -0300

    update

---
+++ 6_PROJETO_SECRET_WORD/secretword/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Maçã', 'Pêra', 'Mamão', 'Laranja'],
+    corpo: ['Braço', 'Perna', 'Cérebro', 'Pescoço', 'Olhos'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete', 'Fone'],
+    programação: ['Linguagem', 'Framework', 'JavaScript', 'React', 'Python'],
+    alimento: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo'],
+}
