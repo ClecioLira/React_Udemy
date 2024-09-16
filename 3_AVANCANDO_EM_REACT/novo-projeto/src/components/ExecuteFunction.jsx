f3ad6588da5f678f129088caaa8d51c6caa9ff98
Clécio Lira <cclecioc.com@gmail.com>
2024-09-16 10:42:16 -0300

    update curso react

--- 3_AVANCANDO_EM_REACT/novo-projeto/src/components/ExecuteFunction.jsx
+++
@@ -1,9 +0,0 @@
-const ExecuteFunction = ({myFunction}) => {
-    return (
-      <div>
-          <button onClick={myFunction}>Clique aqui</button>
-      </div>
-    )
-  }
-  
-export default ExecuteFunction
