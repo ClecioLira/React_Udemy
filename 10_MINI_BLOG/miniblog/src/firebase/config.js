debb48f5bdece3e9512e86b5e34355d79a9b277f
Clécio Lira <cclecioc.com@gmail.com>
2024-10-08 16:56:34 -0300

    update curso react

---
+++ 10_MINI_BLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ8bplGoIH0GoaeYs1QXGy8gdBbcY-tbc",
+  authDomain: "miniblog-7b159.firebaseapp.com",
+  projectId: "miniblog-7b159",
+  storageBucket: "miniblog-7b159.appspot.com",
+  messagingSenderId: "103535076143",
+  appId: "1:103535076143:web:879ced51de3569475bf7da"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
