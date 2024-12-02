b21d32f5b8c6d787382c4ec03e1079cfa1dae03e
Clécio de Lira Alves <cclecioc.com@gmail.com>
2024-12-02 16:36:33 -0300

    feat: inicio pronto

---
+++ 10_MINI_BLOG/miniblog/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./component/Navbar/Navbar";
+import Footer from "./component/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ 10_MINI_BLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfKcYv0iKW91gU2PUlnDgYRZJmbCLYU2A",
+  authDomain: "miniblog-6b912.firebaseapp.com",
+  projectId: "miniblog-6b912",
+  storageBucket: "miniblog-6b912.firebasestorage.app",
+  messagingSenderId: "993678654571",
+  appId: "1:993678654571:web:5acb2c556371d6f48fc52e",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
