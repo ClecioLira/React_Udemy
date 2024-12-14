4ddd54704f7811e2878cab24f39e25477176618d
Clécio de Lira Alves <cclecioc.com@gmail.com>
2024-12-14 13:38:12 -0300

    feat: funcao de deletar fotos ok

--- 12_REACTGRAM/backend/controllers/PhotoController.js
+++ 12_REACTGRAM/backend/controllers/PhotoController.js
@@ -1,5 +1,5 @@
 const Photo = require("../models/Photo");
-const User = require("../models/User")
+const User = require("../models/User");
 const mongoose = require("mongoose");
 
 // Inserir foto com um usuario relacionado a ela
@@ -21,12 +21,50 @@ const insertPhoto = async (req, res) => {
 
   // Se foto foi criada com sucesso
   if (!newPhoto) {
-    res.status(422).json({errors: ["Houve um problema, por favor tente novamente mais tarde."]})
+    res.status(422).json({
+      errors: ["Houve um problema, por favor tente novamente mais tarde."],
+    });
+    return;
   }
 
-  res.status(201).json(newPhoto)
+  res.status(201).json(newPhoto);
+};
+
+// REMOVER FOTO
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+  const reqUser = req.user;
+
+  try {
+    const photo = await Photo.findById(
+      mongoose.Types.ObjectId.createFromHexString(id)
+    );
+
+    if (!photo) {
+      res.status(404).json({ errors: ["Foto não encontrada."] });
+      return;
+    }
+
+    // Checa se a foto pertence ao usuario logado
+    if (!photo.userId.equals(reqUser._id)) {
+      res.status(422).json({
+        errors: ["Ocorreu um erro, por favor tente novamente mais tarde."],
+      });
+      return;
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    res
+      .status(200)
+      .json({ id: photo._id, message: "Foto excluida com sucesso." });
+  } catch (error) {
+    res.status(404).json({ errors: ["Foto não encontrada."] });
+    return;
+  }
 };
 
 module.exports = {
   insertPhoto,
+  deletePhoto,
 };
