12ce8c8b001f65037f6bdfc344b69cd4bd74b21a
Clécio de Lira Alves <cclecioc.com@gmail.com>
2024-12-16 09:21:08 -0300

    feat: concluindo o registro

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -2,12 +2,20 @@ import "./Auth.css";
 
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// REDUX
+import { register, reset } from "../../slices/authSlice";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +28,13 @@ const Register = () => {
     };
 
     console.log(user);
+
+    dispatch(register(user));
   };
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return (
     <div id="register">
