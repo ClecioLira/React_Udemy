7063b8b221bdcc509e2ce99aabc4d1124c022e5a
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-01-19 13:01:16 -0300

    feat: starting front-end

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -1,31 +1,10 @@
-import "./App.css";
-
-// ROUTER
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-// COMPONENTS
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-
-// PAGES
-import Home from "./pages/Home/Home";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <h1>ReactGram</h1>
     </div>
   );
 }
--- 12_REACTGRAM/frontend/src/index.js
+++ 12_REACTGRAM/frontend/src/index.js
@@ -1,17 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-// REDUX
-import { Provider } from "react-redux";
-import { store } from "./store";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
